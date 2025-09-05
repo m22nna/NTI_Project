ee033f0bfca1df6c1305ab204a1c2e4804dcd496
m22nna <monnaa379@gmail.com>
2025-09-05 21:48:17 +0300

    Add files via upload

---
+++ questions.php
@@ -0,0 +1,149 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/mine.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    
+</head>
+<body>
+    <div class="div border w-75 m-auto mb-5 overflow-visible mt-5">
+       <span class="span main_color border text-white float-end width main_text translate-middle-y me-5"> اسئلة مكررة؟ <span><i class="fa-solid fa-circle-question"></i></span></span>
+        <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 m-auto"  onclick="toggle(this)" data-target="demo">ازاى احجز موعد فى نفس اليوم مع دكتور؟</button>        
+        <p class="main_text mb-3" id="demo" style="display:none;">تستطيع أن تدخل على موقع أو تطبيق فيزيتا وتبحث عن دكاترة في منطقتك، اختار 'اليوم' من المواعيد المتاحة في محدد البحث اعلى الصفحة واحجز في أقل من دقيقة.</p>
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 "  onclick="toggle(this)" data-target="demo1">ازاى اقدر ابحث عن دكتور تابع للتامين الخاص بي؟</button>        
+        <p class="main_text mb-3" id="demo1" style="display:none;">فيزيتا توفر لك فرصة البحث عن دكاترة يقبلون التامين الخاص بك على وجه التحديد. اختار شركه التامين الخاصة بك من محدد البحث اعلى الصفحة واحجز في أقل من دقيقة.</p>
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 "  onclick="toggle(this)" data-target="demo2">ازاى اقدر احجز موعد من على الانترنت مع دكتور؟</button>        
+        <p class="main_text mb-3" id="demo2" style="display:none;">فيزيتا هي خدمه مجانيه على الانترنت، تساعد المرضى في ايجاد دكاترة أو أي تخصص آخر وحجز ميعاد فورا. ثم عند اختيار الموعد المطلوب والمنطقة يمكنك اختيار شركة التامين الخاصة بك اذا توفر لديك. وبناءا على المعلومات التي ادخلتها، ستظهر لك قائمه بكل الدكاترة الذين يطابقون معايير البحث، بالاضافة لقائمة مواعيدهم المتاحة.</p>
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 "  onclick="toggle(this)" data-target="demo3">هل اقدر اختار دكتور حسب الجنس ذكر او نثى؟</button>
+             <p class="main_text mb-3" id="demo3" style="display:none;">عند البحث عن دكاترة على فيزيتا، تستطيع تحديد نتائج البحث الخاصة بك حسب الجنس، بالاضافة لمعايير بحث اخرى. وبهذه الطريقة، سيتم عرض الدكاترة الذين يطابقون تفضيلاتك فقط.</p>        
+        </div>
+          <div class="part w-80 mb-3">
+            <button class="w-100 main_text button_color border mt-3 "  onclick="toggle(this)" data-target="demo4">هل اقدر ابحث عن مواعيد فى نفس اليوم لدكتور؟</button>        
+        <p class="main_text mb-3" id="demo4" style="display:none;">تستطيع استخدام بحث فيزيتا للعثور على دكاترة حسب الاعلى تصنيفا من خلال تقييمات المرضى السابقين الذين قاموا باتمام الزيارة حيث ان في كل مرة يكمل فيها المريض موعد محجوز من على فيزيتا، تتم دعوته لمراجعه وتقييم تجربته وابداء رأيه في الزيارة.</p>
+        </div>
+    </div>
+   
+     <div class="div border w-75 m-auto ">
+       <span class="span main_color border text-white float-end width main_text translate-middle-y me-5"> اعرف اكتر عن صحتك<span><i class="fa-solid fa-circle-question"></i></span></span>
+        <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo5">ايه هو تخصص الطبيب العام؟</button>        
+        <p class="main_text mb-3" id="demo5" style="display:none;">الطبيب العام بيشخص ويعالج المشاكل الصحية الجسدية والنفسية العامة، وبيحول المرضى لـلعلاج العاجل والمتخصص.</p>
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo6">الممارس العام بيعالج امراض ايه؟</button>        
+        <p class="main_text mb-3" id="demo6" style="display:none;">بيعالج المشاكل الصحية العامة زي البرد والسعال والغثيان و الأمراض المزمنة، وبيحول المرضى للعلاج مع دكتور متخصص. الممارس العام بيركز على صحة المريض الجسدية والنفسية.</p>
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo7">ايه اعراض نقص فيتامين د؟</button>        
+        <p class="main_text mb-3" id="demo7" style="display:none;">تشمل أعراض نقص فيتامين د <BR>
+ 
+الإحساس بالتعب والضعف العام<BR>
+ 
+الشعور بالحزن والاكتئاب<BR>
+ 
+الألم العضلي والعظمي<BR>
+ 
+زيادة عدوى الجهاز التنفسي<BR>
+ 
+تغيرات في الشعر والأظافر<BR>
+ 
+تدهور في صحة العظام مما يزيد من خطر الإصابة بكسور<BR></p>
+
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo8">؟ B ايه اعراض نقص فيتامين</button>
+             <p class="main_text mb-3" id="demo8" style="display:none;">من بين الأعراض الشائعة لنقص فيتامين    <BR>
+ 
+فقدان الشهية وفقدان الوزن<BR>
+ 
+الإمساك والإسهال<BR>
+ 
+التعب والضعف العام<BR>
+ 
+العصبية والتوتر والقلق<BR>
+ 
+الصداع والدوار والصدر المتقلص<BR>
+ 
+زيادة ضربات القلب وضيق التنفس<BR>
+ 
+تشنجات العضلات وألم الأعصاب<BR>
+ </p>        
+        </div>
+          <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo12">ايه هى اعراض نقص الكالسيوم؟</button>        
+        <p class="main_text mb-3" id="demo12" style="display:none;">أعراض نقص الكالسيوم تشمل<BR>  
+ 
+آلام العظام والعضلات والمفاصل<BR>
+ 
+الكسور العظمية<BR>
+ 
+تشنجات العضلات<BR>
+ 
+خدران أو وخز في الأطراف<BR>
+ 
+اضطرابات في النوم<BR>
+ 
+القلق والاكتئاب<BR>
+ 
+تساقط الشعر<BR>
+ 
+ضعف الأسنان<BR></p>
+        </div>
+         <div class="part w-80">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="dem9">ايه اعراض النزلة المعوية؟</button>        
+        <p class="main_text mb-3" id="dem9" style="display:none;">:أعراض النزلة المعوية تشمل <BR>
+ 
+الإسهال<BR>
+ 
+الغثيان والقيء<BR>
+ 
+الآلام في البطن<BR>
+ 
+الحمى في بعض الأحيان<BR>
+ 
+التعب والصداع وألم في العضلات<BR></p>
+        </div>
+         <div class="part w-80">
+            <button class="w-100 main_text  border mt-3 mb-1 button_color"  onclick="toggle(this)" data-target="demo10">ايه هو تحليل السكر التراكمي؟</button>        
+        <p class="main_text mb-3" id="demo10" style="display:none;">فحص السكري التراكمي هو تحليل نسبة السكر المرتبط بالهيموجلوبين في الدم، لتقييم فعالية العلاج ومدى التزام المريض به. وينبغي إجراء هذا الاختبار بشكل دوري لمرضى السكري كل 2-3 أشهر. وفي حالة الحصول على نتيجة سكري تراكمي طبيعية، يمكن تقليل عدد الفحوصات إلى مرة كل 6 أشهر. </p>
+        </div>
+         <div class="part w-80 mb-3">
+            <button class="w-100 main_text button_color border mt-3 mb-1"  onclick="toggle(this)" data-target="demo11">انهى ادق الجهاز الزئبقى ام الرقمي؟</button>        
+        <p class="main_text mb-3" id="demo11" style="display:none;">الجهاز الرقمي هو الأكثر دقة بين الجهازين. فالجهاز الزئبقي يستخدم مستويات الزئبق لقياس الضغط، ويمكن أن تؤثر الظروف الخارجية مثل درجة الحرارة والرطوبة على دقة القراءة. بالمقارنة، يستخدم الجهاز الرقمي تقنيات الكترونية لقياس الضغط وليس لديه تأثير كبير من الظروف الخارجية.</p>
+        </div>
+    </div>
+
+    <script>
+   
+function toggle(button) {
+  var targetId = button.getAttribute("data-target");
+  var target = document.getElementById(targetId);
+
+  if (target) {
+    var isHidden = target.style.display === "none" || target.style.display === "";
+    target.style.display = isHidden ? "block" : "none";
+  }
+}
+    </script>
+<script >src="https://kit.fontawesome.com/your-kit-id.js" crossorigin="anonymous"</script>
+    
+</body>
+
+
+
+
+
